544bc3462a90063895e4d109efb17f6d05f64932
arroyo191 <56483281+arroyo191@users.noreply.github.com>
2020-09-07 13:14:30 -0400

    Add files via upload

---
+++ counter/javascript.js
@@ -0,0 +1,15 @@
+var count = 0;
+var number = count;
+
+
+
+
+
+function plus() {
+    document.getElementById('number-display').value = ++count;
+}
+
+function subtract() {
+    document.getElementById('number-display').value = --count;
+}
+
